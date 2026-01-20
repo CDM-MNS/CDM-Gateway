6a9ce090e75366aeda5d4cc985eff4b748554ea3
Theo Sementa <theosementa@gmail.com>
2026-01-20 10:49:34 +0100

    feat: add gateway

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { GlobalGatewayExceptionFilter } from '../exception.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalFilters(new GlobalGatewayExceptionFilter());
+  await app.listen(process.env.API_PORT ?? 0);
 }
 bootstrap();
